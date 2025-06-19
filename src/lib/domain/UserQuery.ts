114e0dcd43b77b7845d2f9ad100001179fea1ba0
CoCo9122 <banaji.yu0517@outlook.jp>
2025-06-19 18:36:28 +0900

    プロフィール変更の設定

--- src/lib/domain/UserQuery.ts
+++ src/lib/domain/UserQuery.ts
@@ -7,7 +7,15 @@ export class User {
 		public email: string,
 		public avatarUrl: string,
 		public lastLoginAt: Date | null,
-		public bio?: string, // ユーザーの自己紹介やプロフィール情報
+		public bio: string | null, // ユーザーの自己紹介やプロフィール情報
+		public servingSize: number | null, // ユーザーの料理のサービングサイズ（オプション）
+		public saltPreference: string | null, // ユーザーの塩分の好み（オプション）
+		public sweetnessPreference: string | null, // ユーザーの甘さの好み（オプション）
+		public spicinessPreference: string | null, // ユーザーの辛さの好み（オプション）
+		public cookingTimePreference: string | null, // ユーザーの料理時間の好み（オプション）
+		public mealPurpose: string | null, // ユーザーの食事の目的（オプション）
+		public dislikedIngredients: string | null, // ユーザーが嫌いな食材のリスト（オプション）
+		public preferenceTrend: string | null, // ユーザーの好みのトレンド（オプション）
 	) {}
 }
 
@@ -30,7 +38,15 @@ export interface UserResponse {
 	email: string;
 	avatar_url?: string | null;
 	last_login_at?: string | null; // ISO 8601形式の文字列
-	bio?: string | null; // ユーザーの自己紹介やプロフィール情報
+	bio: string | null; // ユーザーの自己紹介やプロフィール情報
+	serving_size: number | null; // ユーザーの料理のサービングサイズ（オプション）
+	salt_preference: string | null; // ユーザーの塩分の好み（オプション）
+	sweetness_preference: string | null; // ユーザーの甘さの好み（オプション）
+	spiciness_preference: string | null; // ユーザーの辛さの好み（オプション）
+	cooking_time_preference: string | null; // ユーザーの料理時間の好み（オプション）
+	meal_purpose: string | null; // ユーザーの食事の目的（オプション）
+	disliked_ingredients: string | null; // ユーザーが嫌いな食材のリスト（オプション）
+	preference_trend: string | null; // ユーザーの好みのトレンド（オプション）
 }
 
 export interface UserRecipeResponse {
@@ -56,7 +72,15 @@ export interface UserUpdateRequest {
 	password?: string;
 	avatar_url?: string | null;
 	last_login_at?: string | null;
-	bio?: string | null;
+	bio: string | null;
+	serving_size?: number | null;
+	salt_preference?: string | null;
+	sweetness_preference?: string | null;
+	spiciness_preference?: string | null;
+	cooking_time_preference?: string | null;
+	meal_purpose?: string | null;
+	disliked_ingredients?: string | null; // ユーザーが嫌いな食材のリスト（オプション）
+	preference_trend?: string | null;
 }
 
 export interface UserRecipeRequest {
@@ -73,6 +97,14 @@ function createUser(res: UserResponse): User {
 		res.avatar_url || "", // アバターURLがnullの場合はnullを設定
 		res.last_login_at ? new Date(res.last_login_at) : null,
 		res.bio || "", // ユーザーの自己紹介やプロフィール情報
+		res.serving_size || null, // ユーザーの料理のサービングサイズ（オプション）
+		res.salt_preference || "", // ユーザーの塩分の好み（オプション）
+		res.sweetness_preference || "", // ユーザーの甘さの好み（オプション）
+		res.spiciness_preference || "", // ユーザーの辛さの好み（オプション）
+		res.cooking_time_preference || "", // ユーザーの料理時間の好み（オプション）
+		res.meal_purpose || "", // ユーザーの食事の目的（オプション）
+		res.disliked_ingredients || "", // ユーザーが嫌いな食材のリスト（オプション）
+		res.preference_trend || "", // ユーザーの好みのトレンド（オプション）
 	);
 }
 
